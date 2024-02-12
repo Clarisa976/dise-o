16e6470c7492b2e1cba874190af0bd4413b2bd1c
clara <calvara976@g.educaand.es>
2024-02-12 22:40:30 +0100

    :D

--- jquery/tareasEventos6/js/funcionamiento.js
+++ jquery/tareasEventos6/js/funcionamiento.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
-	$("header").scroll(function () {
-		$(this).css({
-			"background-color": "lightpink",
-			"font-size":"20px"
-		})
+	$(window).scroll(function() {
+        if ($(this).scrollTop() > 0) {//si la posición es mayor a 0 se ha desplazado hacia abajo
+            $("header").addClass("fijar-cabecera");//se añade la clase para fijar la cabecera y ponerla un poco transparente
+        } else {
+            $("header").removeClass("fijar-cabecera");//sino se quita
+        }
 	});
+
+	$(window).resize(function() {
+		if($(window).width()>1200){//tamaño puesto para la versión de escritorio
+				$("#menu-hamburger").prop("checked", true);//se marca el checkbox del menú de hamburguesa
+		}else{
+			$("#menu-hamburger").prop("checked", false);//se desmarca el checkbox del menú de hamburguesa
+		}
+                 
+    });
 	
 })
