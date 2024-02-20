69239f82d1a5ed2edcf692d7f2427c8c848b63d0
clara <calvara976@g.eduacaand.es>
2024-02-20 14:36:40 +0100

    morirse

---
+++ jquery/pruebaCreacionSlider/mislider.js
@@ -0,0 +1,118 @@
+$(document).ready(function () {
+    var SliderModule = (function () {
+        var pb = {};//creamos un objeto
+        //almacenamos nuestro #slider en el atributo elslider
+        pb.elslider = $('#slider');
+        //el atributo item es un array que almacena los paneles del slider
+        pb.items = {
+            panels: pb.elslider.find(".slider-wrapper > li"),
+        }
+        //intervalo del Slider
+        var SliderInterval,
+            currentSlider = 0,//actual por defecto en 0
+            nextSlider = 1,//el siguiente por defecto es 1
+            lengthSlider = pb.items.panels.length;//la longitud
+
+        //constructor del Slider
+        pb.init = function (settings) {
+            this.settings = settings || {duration:3000}
+            var loscontroles = '';
+            //console.log("Inicializado");
+            SliderInit();//para inicializar el slider
+            
+            //creamos los controles del Slider en tiempo de ejecución
+            for(var i=0;i<lengthSlider;i++){
+                if(i==0){
+                    loscontroles += '<li class="active"></li>';
+                }else{
+                    loscontroles += '<li></li>';
+                }
+            }
+            //insertamos los controles creados en el  html
+            $('#control-buttons').html(loscontroles);
+            
+
+            $('#control-buttons li').click(function () {
+
+                //al hacer clic vemos en la consola el índice                
+                //console.log($(this).index());
+                console.log(currentSlider);
+                console.log($(this).index());
+
+                if (currentSlider !== $(this).index()) {
+                    cambiarPanel($(this).index());
+                }
+                //cambiarPanel($(this).index());
+            });
+        }
+
+        //funcioón que inicializa el Slider
+        var SliderInit = function () {
+            //SliderInterval = setInterval(pb.startSlider, 3000);//intervalo de ejecución
+            SliderInterval = setInterval(pb.startSlider, pb.settings.duration);
+        }
+
+        pb.startSlider = function () {
+            var paneles = pb.items.panels;//cogemos los paneles del slider
+            var controles = $('#control-buttons li');//creamos variable
+            //console.log('Mensaje');
+
+            //controlamos si nos encontramos en el último panel 
+            if (nextSlider >= lengthSlider) {
+                nextSlider = 0;
+                currentSlider = lengthSlider - 1;
+            }
+            //efectos
+            //eliminamos la clase en todos los controles
+            controles.removeClass('active');
+            //añadimos la clase al control del panel seleccionado
+            controles.eq(nextSlider).addClass('active');
+
+            paneles.eq(currentSlider).fadeOut('slow');//aplicamos efectos de transición
+            paneles.eq(nextSlider).fadeIn('slow');
+
+            //console.log(nextSlider);
+            //actualizamos las variables
+            currentSlider = nextSlider;
+            nextSlider += 1;
+        }
+
+        //función para los controles del Slider
+        //recibe el índice del panel a mostrar
+        var cambiarPanel = function (indice) {
+            //limpiar el intervalo previamente.
+            clearInterval(SliderInterval);
+
+            var paneles = pb.items.panels;
+            var controles = $('#control-buttons li');
+            //comprobamos que el índice esté disponible
+            //dentro de los paneles del slider.
+            if (indice >= lengthSlider) {
+                indice = 0; //para que no se pase de la cantidad de imágenes
+            } else if (indice < 0) {
+                indice = lengthSlider - 1;
+            }
+            //EFECTOS
+            //eliminamos la clase en todos los controles
+            controles.removeClass('active');
+            //añadimos la clase al control del panel seleccionado
+            controles.eq(indice).addClass('active');
+            //efectos de transición
+            paneles.eq(currentSlider).fadeOut('slow');
+            //el siguiente panel del slider es el que indique
+            //el parámetro "indice"
+            paneles.eq(indice).fadeIn('slow');
+
+            //actualizamos los datos 
+            currentSlider = indice;
+            nextSlider = indice + 1;
+
+            //reactivar el Slider
+            SliderInit();
+        }
+
+        return pb;//devolvemos el objeto pb
+    }());//()) es para que se ejecute automáticamente
+    //llamada al constructor
+    SliderModule.init();
+})
