c0fe53aa1c1aaf9b22b0c54ad15da6d090923871
clara <clarisa21h@gmail.com>
2024-03-04 11:35:28 +0100

    ..

---
+++ jquery/tareaWebJUSTFAB/Webusefab/jq/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    /*mostrar y ocultar el menú*/ 
+    $("div#desplazable > header#top > div > nav#menu-principal").on({
+        click: function () {
+            $("ul#menu").animate({ left: "0" }, 300);
+            $("div#desplazable").animate({ left: "15rem" }, 300);
+            $("div#oscuro").css("display", "block");
+        }
+    });
+    $("div#oscuro").on({
+        click: function () {
+            $("ul#menu").animate({ left: "-18rem" }, 300);
+            $("div#desplazable").animate({ left: "0" }, 300);
+            $("div#oscuro").css("display", "none");
+        }
+    });
+    
+
+    /*mostramos el botón de comprar*/
+    $("article.item").on({
+        mouseenter: function(){
+            $(this).children("a").children("span.comprar").css("display", "block");
+        },
+        mouseleave: function () {
+            $(this).children("a").children("span.comprar").css("display", "none");
+        }
+    });
+
+    
+});
